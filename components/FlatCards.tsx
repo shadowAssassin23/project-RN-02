9abb09e163cfd787b038efde80b69b110c9d697c
shadowAssassin23 <fighterb683@gmail.com>
2024-09-10 19:09:22 +0530

    add FlatCards

---
+++ components/FlatCards.tsx
@@ -0,0 +1,51 @@
+import { StyleSheet, Text, useColorScheme, View } from 'react-native'
+import React from 'react'
+
+export default function FlatCards() {
+    return (
+        <View>
+            <Text style={styles.headingText}>FlatCards</Text>
+            <View style={styles.container}>
+                <View style = {[styles.card,styles.cardOne]}>
+                    <Text>Red</Text>
+                </View>
+                <View style = {[styles.card,styles.cardTwo]}>
+                    <Text>Green</Text>
+                </View>
+                <View style = {[styles.card,styles.cardThree]}>
+                    <Text>Blue</Text>
+                </View>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    headingText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    container: {
+        flex:1,
+        flexDirection: 'row'
+    },
+    card : {
+        flex : 1,
+        alignItems: 'center',
+        justifyContent : 'center',
+        width: 100,
+        height: 100,
+        borderRadius: 10,
+        margin:8
+    },
+    cardOne : {
+        backgroundColor: '#E44236',
+        marginLeft:0,
+    },
+    cardTwo : {
+        backgroundColor: '#45CE30'
+    },
+    cardThree : {
+        backgroundColor: '#01CBC6'
+    },
+})
